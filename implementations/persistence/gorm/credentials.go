33c2aca69a672f6f0066506b5df80e65874ff815
Luis masuelli <luismasuelli@hotmail.com>
2019-05-11 15:38:48 -0500

    (Still abstract) gorm and xorm base credentials

--- implementations/persistence/gorm/credentials.go
+++ implementations/persistence/gorm/credentials.go
@@ -1,1 +1,41 @@
 package gorm
+
+import (
+	"github.com/luismasuelli/go-identity/interfaces"
+	"github.com/luismasuelli/go-identity/types"
+	"github.com/jinzhu/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Username string `gorm:"size:30;not null;unique"`
+	Password string `gorm:"size:100;not null"`
+}
+
+func (*User) IdentificationField() string {
+	return "username"
+}
+
+func (*User) IdentificationIsCaseSensitive() bool {
+	return false
+}
+
+func (user *User) SetHashedPassword(password string) {
+	user.Password = password
+}
+
+func (user *User) ClearPassword() {
+	user.SetHashedPassword("")
+}
+
+func (user *User) HashedPassword() string {
+	return user.Password
+}
+
+func (user *User) HashingEngine() interfaces.PasswordHashingEngine {
+	panic("not implemented")
+}
+
+func (user *User) CheckLogin(stage types.LoginStage) error {
+	return nil
+}
