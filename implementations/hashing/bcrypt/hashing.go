50e2b57b3a95e1051c4b059f57df8f12ac843d74
Luis masuelli <luismasuelli@hotmail.com>
2019-05-11 14:51:36 -0500

    Adding bcrypt hashing facade

---
+++ implementations/hashing/bcrypt/hashing.go
@@ -0,0 +1,20 @@
+package bcrypt
+
+import "golang.org/x/crypto/bcrypt"
+
+type BCryptHashingEngine struct {
+	cost int
+}
+
+func (bcryptHashingEngine *BCryptHashingEngine) Hash(password string) (string, error) {
+	result, err := bcrypt.GenerateFromPassword([]byte(password), bcryptHashingEngine.cost)
+	if err != nil {
+		return "", err
+	} else {
+		return string(result), err
+	}
+}
+
+func (bcryptHashingEngine *BCryptHashingEngine) Validate(password string, hash string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
