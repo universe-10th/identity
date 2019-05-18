7379102afa6b3d5da78a7ebe8a51597238f13360
Luis masuelli <luismasuelli@hotmail.com>
2019-05-17 23:47:53 -0500

    Migrations fixed

--- implementations/persistence/gorm/scoped/migration.go
+++ implementations/persistence/gorm/scoped/migration.go
@@ -1,7 +1,12 @@
-package bare
+package scoped
 
 import "github.com/jinzhu/gorm"
 
 func MigrateCredentials(db *gorm.DB) {
 	db.AutoMigrate(&User{})
+}
+
+
+func MigrateScopes(db *gorm.DB) {
+	db.AutoMigrate(&ModelBackedScope{})
 }
