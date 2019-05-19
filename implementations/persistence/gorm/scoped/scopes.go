705edb5a924af00a5ae55ee7bf170ffc7dbbf726
Luis masuelli <luismasuelli@hotmail.com>
2019-05-18 23:06:29 -0500

    Documenting the ModelBackedScope class

--- implementations/persistence/gorm/scoped/scopes.go
+++ implementations/persistence/gorm/scoped/scopes.go
@@ -4,37 +4,52 @@ import (
 	"github.com/universe-10th/identity/stub"
 )
 
-/**
- * Scope implementation for GORM engine.
- */
+
+// This is an implementation of the Scope interface
+// consisting on database-stored instances. There
+// will no more satisfaction checks than the scope
+// belonging to a scope map in the related credential,
+// who must also implement ModelBackedScopeHolder
+// interface and contain this scope in the map for
+// the satisfaction question to be answered as true.
 type ModelBackedScope struct {
-	Id uint `gorm:"primary_key"`
-	Code string `gorm:"size:30;not null;unique"`
-	Label string `gorm:"size:50;not null"`
+	Id       uint   `gorm:"primary_key"`
+	Code     string `gorm:"size:30;not null;unique"`
+	Label    string `gorm:"size:50;not null"`
 	HelpText string `gorm:"type:text;not null"`
 }
 
-/**
- * A scope holder is an interface (intended for the
- *   credentials) returning associated instances of
- *   ModelBackedScope for it.
- */
+
+// Credential implementations must implement this
+// interface if they expect ModelBackedScope instances
+// return true in the satisfaction question.
 type ModelBackedScopeHolder interface {
 	GetScopes(forceRefresh bool) map[string]*ModelBackedScope
 }
 
+
+// The scope's key is stored in its "key" field.
 func (scope *ModelBackedScope) Key() string {
 	return scope.Code
 }
 
+
+// The scope's name is stored in its "name" field.
 func (scope *ModelBackedScope) Name() string {
 	return scope.Label
 }
 
+
+// The scope's description is stored in its "description"
+// field.
 func (scope *ModelBackedScope) Description() string {
 	return scope.HelpText
 }
 
+
+// Satisfaction check against a credential implies the
+// credential implements ModelBackedScopeHolder and also
+// contains the current scope among their scopes.
 func (scope *ModelBackedScope) SatisfiedBy(credential stub.Credential) bool {
 	if holder, isHolder := credential.(ModelBackedScopeHolder); isHolder {
 		_, ok := holder.GetScopes(false)[scope.Key()]
