7e319ea090cbe3f2a0fdac5be3470f27528d8b42
Luis masuelli <luismasuelli@hotmail.com>
2019-05-23 23:54:29 -0500

    Moving login method inside MultiRealm.

--- realm.go
+++ realm.go
@@ -3,6 +3,7 @@ package identity
 import (
 	"github.com/universe-10th/identity/stub"
 	"reflect"
+	"github.com/universe-10th/identity/support/types"
 )
 
 
@@ -107,3 +108,45 @@ func (multiRealm MultiRealm) MultiLookup(identification interface{}) (string, st
 	return "", nil, NoMultiMatch
 }
 
+
+// Given a realm code, an identification and a password, it tries to perform a login.
+// It may fail for several reasons: a database error, an unmatched lookup, a bad password, a
+// password-less credential, or another custom login error (after or before the passwords check).
+func (multiRealm MultiRealm) Login(realm string, identification interface{}, password string) (string, stub.Credential, error) {
+	var credential stub.Credential
+	var err error
+
+	// Perform the lookup
+	if realm == "" {
+		realm, credential, err = multiRealm.MultiLookup(identification)
+	} else {
+		credential, err = multiRealm.Lookup(realm, identification)
+	}
+	if err != nil {
+		return "", nil, err
+	}
+
+	// Check if it has password
+	hash := credential.HashedPassword()
+	if hash == "" {
+		return "", nil, CredentialDoesNotHavePassword
+	}
+
+	// Check on "before" stage
+	if err := credential.CheckLogin(types.BeforePasswordCheck); err != nil {
+		return "", nil, err
+	}
+
+	// Check the password
+	if err := credential.HashingEngine().Validate(password, hash); err != nil {
+		return "", nil, err
+	}
+
+	// Check on "after" stage
+	if err := credential.CheckLogin(types.AfterPasswordCheck); err != nil {
+		return "", nil, err
+	}
+
+	// Succeed: return realm key, credential, and no error
+	return realm, credential, nil
+}
