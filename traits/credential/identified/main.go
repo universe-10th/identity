1976e7f76e560f96b1c737bfcbf05b0c50362fe7
luismasuelli <luis@triplemint.com>
2020-04-24 14:51:39 -0500

    Moving the traits

--- traits/credential/identified/main.go
+++ traits/credential/identified/main.go
@@ -1,10 +1,9 @@
-package traits
-
+package identified
 
 // This trait knows its identification
 // by its own. By contract, must return
 // the identification a related source
 // would use to retrieve this instance.
-type SelfIdentified interface {
+type Identified interface {
 	Identification() interface{}
 }
--- traits/credential/indexed/main.go
+++ traits/credential/indexed/main.go
@@ -1,10 +1,9 @@
-package traits
-
+package indexed
 
 // This trait knows its lookup index by
 // its own. By contract, must return the
 // lookup index a related source would
 // use to retrieve this instance.
-type SelfIndexed interface {
+type Indexed interface {
 	Index() interface{}
 }
