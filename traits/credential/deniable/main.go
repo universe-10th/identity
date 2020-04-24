db5bb679a630d921429e07de7744331c24b2d514
luismasuelli <luis@triplemint.com>
2020-04-24 17:50:05 -0500

    Changing punish contract.

--- traits/credential/deniable/main.go
+++ traits/credential/deniable/main.go
@@ -7,14 +7,14 @@ import (
 
 // This trait allows to punish a credential (this implies the
 // credential should fail to log in for certain duration or
-// perhaps permanently). If the first return value is false,
-// the credential is not punished. If true, the fourth return
-// value will hold the reason. Also, if true and the second
-// value is nil, this means a permanent punish. The fifth result
+// perhaps permanently). If the first return value is nil,
+// the credential is not punished. If not, the third return
+// value will hold the reason. Also, if not nil and the second
+// value is nil, this means a permanent punish. The fourth result
 // value may be nil. Punishing an already punished credential
 // will entirely replace the ban.
 type Punishable interface {
-	PunishedFor() (banned bool, forTime *time.Duration, reason interface{}, by identity.Credential)
+	PunishedFor() (punishedOn *time.Time, forTime *time.Duration, reason interface{}, by identity.Credential)
 	Punish(forTime *time.Duration, reason interface{}, by identity.Credential)
 	Unpunish()
 }
