0ed688fd293246b8276d45dbb39edefeeb1fc147
luismasuelli <luis@triplemint.com>
2020-04-24 17:02:46 -0500

    Adding a function to create the "scoped" authorization requirement.

--- authreqs/scoped/main.go
+++ authreqs/scoped/main.go
@@ -13,7 +13,7 @@ type Scoped struct {
 }
 
 // Tests whether the credential is superuser or not.
-func (scoped Scoped) SatisfiedBy(credential identity.Credential) bool {
+func (scoped *Scoped) SatisfiedBy(credential identity.Credential) bool {
 	if scopedCredential, ok := credential.(scoped2.Scoped); ok && scoped.tree.SatisfiedBy(scopedCredential) {
 		return true
 	} else {
@@ -21,3 +21,12 @@ func (scoped Scoped) SatisfiedBy(credential identity.Credential) bool {
 	}
 }
 
+// Creates a new Scoped requirement checking all the
+// given Scopes or tree nodes (i.e. Any() or All())
+// in an "any" way. This means: alternative scopes
+// or nodes will be tried and one of them must be
+// entirely satisfied. Intended for main user/player
+// interaction with the server.
+func RequireScopesAmong(scopes ...ScopeSpec) *Scoped {
+	return &Scoped{Any(scopes...)}
+}
