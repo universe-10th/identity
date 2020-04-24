75139fec04e81f9b7980a08983d5b341cdbe01f9
luismasuelli <luis@triplemint.com>
2020-04-24 17:07:50 -0500

    Adding ALL the compound auth requirements.

--- authreqs/compound/main.go
+++ authreqs/compound/main.go
@@ -1,35 +1,41 @@
 package compound
 
-import "github.com/universe-10th/identity"
+import (
+	"github.com/universe-10th/identity"
+	"github.com/universe-10th/identity/authreqs/scoped"
+	"github.com/universe-10th/identity/authreqs/superuser"
+	"github.com/universe-10th/identity/authreqs/staff"
+)
 
-// This requirement implements satisfaction
-// by checking and requiring all of the composed
-// (children) requirements.
-type AllOf []identity.AuthorizationRequirement
-
-// Considers satisfaction by requiring all the
-// inner requirements to be satisfied.
-func (allOf AllOf) SatisfiedBy(credential identity.Credential) bool {
-	for _, value := range allOf {
-		if !value.SatisfiedBy(credential) {
-			return false
+// Returns an authorizer function which tells
+// whether the credential was authorized by
+// trying different authorization requirements
+// until one succeeds.
+func TryAll(alternatives ...identity.AuthorizationRequirement) func(identity.Credential) bool {
+	return func(credential identity.Credential) bool {
+		for _, alternative := range alternatives {
+			if alternative.SatisfiedBy(credential) {
+				return true
+			}
 		}
+		return false
 	}
-	return true
 }
 
-// This requirement implements satisfaction
-// by checking and requiring all of the composed
-// (children) requirements.
-type AnyOf []identity.AuthorizationRequirement
+// Returns a superuser/scoped combined authorizer
+// which tells whether the credential is superuser.
+// This is not useful for user-end features but
+// only for administrative features.
+func Admin(scopes ...scoped.ScopeSpec) func(identity.Credential) bool {
+	superuserReq := superuser.RequireSuperuser
+	staffReq := staff.RequireStaff
+	scopedReq := scoped.RequireScopesAmong(scopes...)
 
-// Considers satisfaction by requiring any of
-// the inner requirements to be satisfied.
-func (anyOf AnyOf) SatisfiedBy(credential identity.Credential) bool {
-	for _, value := range anyOf {
-		if value.SatisfiedBy(credential) {
+	return func(credential identity.Credential) bool {
+		if superuserReq.SatisfiedBy(credential) {
 			return true
+		} else {
+			return staffReq.SatisfiedBy(credential) && scopedReq.SatisfiedBy(credential)
 		}
 	}
-	return false
 }
