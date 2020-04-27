6a526f7f6148b3c8d558412572b74b7274b54670
luismasuelli <luis@triplemint.com>
2020-04-27 14:05:00 -0500

    Adding the source interface.

---
+++ login/sources/main.go
@@ -0,0 +1,11 @@
+package sources
+
+import "github.com/universe-10th/identity/credentials"
+
+// Retrieves a credential, or an error, by its identifier.
+// By contract, and security reasons, if the credential is
+// not found, implementors should return login.ErrLoginFailed,
+// with a null credential.
+type LoginSource interface {
+	ByIdentifier(identifier interface{}) (credentials.Credential, error)
+}
