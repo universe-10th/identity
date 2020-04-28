ae7d30575b72467f3b5fe118205058c8f52ad20a
Luis Masuelli <luismasuelli@hotmail.com>
2020-04-27 23:00:20 -0500

    Enhancing the source.

--- credentials/sources.go
+++ credentials/sources.go
@@ -1,12 +1,84 @@
 package credentials
 
-// Sources perform load and save operations over an existing
+import (
+	"errors"
+	"reflect"
+)
+
+// Brokers perform load and save operations over an existing
 // credential (they are not intended to create a new credential).
 // The login operation will involve calling the ByIdentifier
 // method, while the other two are intended for the different
-// password change pipelines.
-type Source interface {
+// password change pipelines. Sources will also be able to tell
+// whether they allow, or not, a given implementation of the
+// Credential interface. This usually involves other traits
+// being implemented in the Credential, and/or things like the
+// credential being a struct-type value.
+type Broker interface {
+	Allows(template Credential) bool
 	ByIdentifier(identifier interface{}, template Credential) (Credential, error)
 	ByIndex(index interface{}, template Credential) (Credential, error)
-	Save(credential Credential)
+	Save(credential Credential) error
+}
+
+// Panicked error when attempting to create a source with a
+// nil broker instead of an instance.
+var ErrNilBroker = errors.New("the given broker is nil")
+
+// Panicked error when attempting to create a source with a
+// nil template credential, or a template not allowed by the
+// chosen broker.
+var ErrBadTemplate = errors.New("the given template is nil or not allowed by the broker")
+
+var ErrNilValueOnSave = errors.New("the credential being saved is nil")
+
+// Returned error when the credential being saved is of a
+// different type than the one of the source.
+var ErrBadTypeOnSave = errors.New("the credential being saved is of a different type")
+
+// Sources are a combination of an existing broker instance and
+// a non-nil Credential instance that will serve as template.
+// Sources will proxy the calls to a broker, and also will be
+// able to instantiate dummy objects of the same type of the
+// given template.
+type Source struct {
+	broker   Broker
+	template Credential
+	tmplType reflect.Type
+}
+
+// Creates a new source for a given broker and template, if they
+// match together. Panics if either is nil, or the broker does
+// not allow it.
+func NewSource(broker Broker, template Credential) *Source {
+	if broker == nil {
+		panic(ErrNilBroker)
+	} else if template == nil || !broker.Allows(template) {
+		panic(ErrBadTemplate)
+	}
+	return &Source{broker, template, reflect.TypeOf(template)}
+}
+
+// Bypasses its implementation to the broker but using the chosen
+// template instance.
+func (source *Source) ByIdentifier(identifier interface{}) (Credential, error) {
+	return source.broker.ByIdentifier(identifier, source.template)
+}
+
+// Bypasses its implementation to the broker but using the chosen
+// template instance.
+func (source *Source) ByIndex(index interface{}) (Credential, error) {
+	return source.broker.ByIndex(index, source.template)
+}
+
+// Bypasses its implementation to the broker but using the chosen
+// template instance, adding a check type.
+func (source *Source) Save(credential Credential) error {
+	if credential == nil {
+		return ErrNilValueOnSave
+	} else if reflect.TypeOf(credential) != source.tmplType {
+		return ErrBadTypeOnSave
+	} else {
+		return source.broker.Save(credential)
+	}
 }
