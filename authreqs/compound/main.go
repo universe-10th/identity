fe798240256ec5b61df3d6277e4a3f8ec47a196e
Luis Masuelli <luismasuelli@hotmail.com>
2020-04-27 22:39:10 -0500

    Replacing the admin function with a class.

--- authreqs/compound/main.go
+++ authreqs/compound/main.go
@@ -12,6 +12,7 @@ import (
 // and succeeds when one of them succeeds.
 type TryAll []authreqs.AuthorizationRequirement
 
+// Performs the test one-by-one until one succeeds.
 func (tryAll TryAll) SatisfiedBy(credential credentials.Credential) bool {
 	for _, alternative := range tryAll {
 		if alternative.SatisfiedBy(credential) {
@@ -21,20 +22,31 @@ func (tryAll TryAll) SatisfiedBy(credential credentials.Credential) bool {
 	return false
 }
 
+// Intended for admin side, it tests whether the
+// credential is superuser, or a staff satisfying
+// the required permissions, if any.
+type AdminRequirement struct {
+	superuserReq superuser.Superuser
+	staffReq     staff.Staff
+	scopedReq    *scoped.Scoped
+}
+
+// Performs the test (superuser || staff && permissions).
+func (adminRequirement *AdminRequirement) SatisfiedBy(credential credentials.Credential) bool {
+	if adminRequirement.superuserReq.SatisfiedBy(credential) {
+		return true
+	} else {
+		return adminRequirement.staffReq.SatisfiedBy(credential) && adminRequirement.scopedReq.SatisfiedBy(credential)
+	}
+}
+
 // Returns a superuser/scoped combined authorizer
 // which tells whether the credential is superuser.
 // This is not useful for user-end features but
 // only for administrative features.
-func Admin(scopes ...scoped.ScopeSpec) func(credentials.Credential) bool {
+func Admin(scopes ...scoped.ScopeSpec) *AdminRequirement {
 	superuserReq := superuser.RequireSuperuser
 	staffReq := staff.RequireStaff
 	scopedReq := scoped.RequireScopesAmong(scopes...)
-
-	return func(credential credentials.Credential) bool {
-		if superuserReq.SatisfiedBy(credential) {
-			return true
-		} else {
-			return staffReq.SatisfiedBy(credential) && scopedReq.SatisfiedBy(credential)
-		}
-	}
+	return &AdminRequirement{superuserReq, staffReq, scopedReq}
 }
