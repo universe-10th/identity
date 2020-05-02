3705c583a0e42fe63055fb92875e9d22fe278f00
Luis Masuelli <luismasuelli@hotmail.com>
2020-05-01 22:52:36 -0500

    Adding the dummy hasher.

---
+++ samples/dummy_hasher.go
@@ -0,0 +1,17 @@
+package samples
+
+import "errors"
+
+type DummyHasher uint
+
+func (DummyHasher) Name() string { return "dummy" }
+func (DummyHasher) Hash(password string) (string, error) {
+	return "Hashed[" + password + "]", nil
+}
+func (h DummyHasher) Validate(password string, hash string) error {
+	if h, _ := h.Hash(password); h == hash {
+		return nil
+	} else {
+		return errors.New("bad password")
+	}
+}
