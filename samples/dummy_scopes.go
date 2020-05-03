ac34fd6531103725a733e2e97be0e7a53fc446c4
Luis Masuelli <luismasuelli@hotmail.com>
2020-05-02 22:50:19 -0500

    Adding the dummy scopes.

---
+++ samples/dummy_scopes.go
@@ -0,0 +1,17 @@
+package samples
+
+import "fmt"
+
+type DummyScope uint8
+
+func (scope DummyScope) Key() string {
+	return fmt.Sprintf("scope%d", scope)
+}
+
+func (scope DummyScope) Name() string {
+	return fmt.Sprintf("Scope %d", scope)
+}
+
+func (scope DummyScope) Description() string {
+	return fmt.Sprintf("Sample scope %d", scope)
+}
