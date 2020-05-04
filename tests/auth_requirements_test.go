3b69fbe93f29b666e2d3e336f9f822376acb99b1
Luis Masuelli <luismasuelli@hotmail.com>
2020-05-04 12:42:16 -0500

    All of the auth requirement tests.

--- tests/auth_requirements_test.go
+++ tests/auth_requirements_test.go
@@ -51,17 +51,49 @@ func TestAdminNothing(t *testing.T) {
 }
 
 func TestTryAllIsSuperuser(t *testing.T) {
+	requirements, realms := MakeUserExampleInstances()
+	adminRealm := realms[0]
+	tryAll := requirements[2]
 
+	if credential, err := adminRealm.Login("SU", "admin-su$123"); err != nil {
+		t.Errorf("This login must not fail! Error received: %s\n", err)
+	} else if !tryAll.SatisfiedBy(credential) {
+		t.Error("SU account must satisfy the (superuser, scope 2, scope 7) 'try all' requirement")
+	}
 }
 
 func TestTryAllSatisfiesScopes(t *testing.T) {
+	requirements, realms := MakeUserExampleInstances()
+	adminRealm := realms[0]
+	tryAll := requirements[2]
 
+	if credential, err := adminRealm.Login("S1", "admin-s1$123"); err != nil {
+		t.Errorf("This login must not fail! Error received: %s\n", err)
+	} else if !tryAll.SatisfiedBy(credential) {
+		t.Error("S1 account must satisfy the (superuser, scope 2, scope 7) 'try all' requirement")
+	}
 }
 
 func TestTryAllDoesNotSatisfyScopes(t *testing.T) {
+	requirements, realms := MakeUserExampleInstances()
+	adminRealm := realms[0]
+	tryAll := requirements[2]
 
+	if credential, err := adminRealm.Login("S3", "admin-s3$123"); err != nil {
+		t.Errorf("This login must not fail! Error received: %s\n", err)
+	} else if tryAll.SatisfiedBy(credential) {
+		t.Error("S3 account must NOT satisfy the (superuser, scope 2, scope 7) 'try all' requirement")
+	}
 }
 
 func TestTryAllNothing(t *testing.T) {
+	requirements, realms := MakeUserExampleInstances()
+	userRealm := realms[1]
+	tryAll := requirements[2]
 
+	if credential, err := userRealm.Login("U1", "user1$123"); err != nil {
+		t.Errorf("This login must not fail! Error received: %s\n", err)
+	} else if tryAll.SatisfiedBy(credential) {
+		t.Error("U1 account must NOT satisfy the (superuser, scope 2, scope 7) 'try all' requirement")
+	}
 }
