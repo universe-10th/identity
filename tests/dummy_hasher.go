9e73a0902ec3ba4ad75d447aaed88f21cb1f5fd6
Luis Masuelli <luismasuelli@hotmail.com>
2020-05-04 09:17:25 -0500

    Making the dummy hasher available as different instances (different hasher names).

--- tests/dummy_hasher.go
+++ tests/dummy_hasher.go
@@ -1,15 +1,18 @@
 package tests
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 type DummyHasher uint
 
-func (DummyHasher) Name() string { return "dummy" }
-func (DummyHasher) Hash(password string) (string, error) {
-	return "Hashed[" + password + "]", nil
+func (hasher DummyHasher) Name() string { return fmt.Sprintf("dummy[%d]", hasher) }
+func (hasher DummyHasher) Hash(password string) (string, error) {
+	return fmt.Sprintf("Hashed[%s, %d]", password, hasher), nil
 }
-func (h DummyHasher) Validate(password string, hash string) error {
-	if h, _ := h.Hash(password); h == hash {
+func (hasher DummyHasher) Validate(password string, hash string) error {
+	if h, _ := hasher.Hash(password); h == hash {
 		return nil
 	} else {
 		return errors.New("bad password")
