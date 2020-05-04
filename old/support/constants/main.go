101f12a285b07b79b081e719f27210a448762178
Luis Masuelli <luismasuelli@hotmail.com>
2020-05-04 12:45:52 -0500

    Removing the old implementation.

--- old/support/constants/main.go
+++
@@ -1,8 +0,0 @@
-package constants
-
-const (
-    Namespace  string = "universe-10th/identity"
-    ID         string = Namespace + ":ID"
-    Realm      string = Namespace + ":Realm"
-    Credential string = Namespace + ":Credential"
-)
--- old/support/types/login_stages.go
+++
@@ -1,9 +0,0 @@
-package types
-
-
-// Login check stages (to add custom checks).
-type LoginStage int
-const (
-	BeforePasswordCheck = LoginStage(0)
-	AfterPasswordCheck = LoginStage(1)
-)
